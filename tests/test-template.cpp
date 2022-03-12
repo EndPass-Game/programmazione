ad536fd380c7b85469d53ed715a209ce5a8b2204
Angelo Huang <huangelo02@gmail.com>
2022-03-12 11:42:14 +0100

    test: create test table and runner
    Now you can register the unit test functions in a table in each test file. This table has the function pointer and the name of the test as input (better to have same name of the func
    furthermore there is a metafunction that runs the test functions.

--- tests/test-template.cpp
+++ tests/test-template.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <cassert>
+#include <assert.h>
 
 // Questo è un piccolo template per i test. 
 // Ci sarebbero delle feature che sarebbe bello se si potesse implementare: 
@@ -11,15 +11,36 @@
 // 4. Una piccola CLI che gestisca ad alto livello le funzioni da chiamare (un pò come
 // cargo test di rust, se si esegue la funzione con un nome di una classe esegue solo
 // un insieme di test ben precisato)
+// AGGIORNAMENTO: 1,2,3 sono più o meno fatti, 2 non`e automatizzato ma utilizza la table
+// 3 basterebbe commentare una entry nella tabella dei test
+
+struct functionMETA {
+    void (*funcPtr) ();
+    const char *funcName;
+};
+
 namespace test {
     void example_test() {
-        // TODO: è molto noioso ripetere questi due cout, inizio e fine, si riesce a generalizzare in una funzione runner?
-        std::cout << "Testando la funzione: " << __func__ << " --- ";
         assert(true); // TODO: Non riesce a darmi un ❌, mi sa che deve essere sovrascritto, un wrapper ad assert?
+    }
+
+    // Wrappa una funzione di test per mostrare l'output di una funzione
+    // se gli assert dentro la funzione di test sono corretti, da input a schermo
+    void run(void (*f) (), const char *name) {
+        std::cout << "Testando la funzione: " << name  << " --- ";
+        f();
         std::cout << "Ok! ✔️" << std::endl;
     }
 }
 
+functionMETA func_table[] = {
+    {test::example_test, "example_test"},
+};
+
+
 int main() {
-    test::example_test();
+    uint n_tests = sizeof(func_table) / sizeof(functionMETA);
+    for (uint i = 0; i < n_tests; i++) {
+        test::run(func_table[i].funcPtr, func_table[i].funcName);
+    }
 }
