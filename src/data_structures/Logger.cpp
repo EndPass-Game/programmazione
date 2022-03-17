2d8a5b3e919eb5a286b7dcbf00900ba97fbbe401
Ruggi54 <76696341+Ruggi54@users.noreply.github.com>
2022-03-17 09:33:39 +0100

    feat(logger): classe per creare i log
    classe che crea i log attraverso un interfaccia statica che si può chiamare con `Logger::log()` e prende gli stessi argomenti di printf

---
+++ src/data_structures/Logger.cpp
@@ -0,0 +1,30 @@
+#include "Logger.hpp"
+
+#include <stdio.h>
+#include <cstring>
+#include <stdarg.h>
+using namespace std;
+
+
+Logger* Logger::instance_=nullptr;
+char* Logger::loggerFileName= "game.log";
+
+FILE* Logger::loggerFile=fopen(loggerFileName, "w");
+
+//Costruttore vuoto della classe
+Logger::Logger(){}
+
+// crea il singleton o restituisce l'oggetto statico
+Logger* Logger::getInstance(){
+    if(instance_ == nullptr){
+        instance_ = new Logger();
+    }
+    return instance_;
+}
+//funzione che scrive nel file il messaggio corrispondente
+void Logger::log(const char *format, ...){
+    va_list args;
+    va_start (args, format);
+    vfprintf (loggerFile, format, args);
+    va_end (args);
+}
