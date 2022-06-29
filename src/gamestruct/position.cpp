12a8cde7e5046f133541488a68426a7c2a4ef70f
Angelo 'Flecart' Huang <huangelo02@gmail.com>
2022-06-29 16:55:19 +0200

    feat(position): add addition subtr to pos, and costructor

--- src/gamestruct/position.cpp
+++ src/gamestruct/position.cpp
@@ -1,18 +1,41 @@
 #include "gamestruct/position.hpp"
 
+Position::Position(int riga, int colonna) {
+    this->riga = riga;
+    this->colonna = colonna;
+}
+
+bool Position::operator!=(const Position& pos) const {
+    return riga != pos.riga || colonna != pos.colonna;
+}
+
+bool Position::operator==(const Position& pos) const {
+    return riga == pos.riga && colonna == pos.colonna;
+}
+
+bool Position::operator<(const Position& pos) const {
+    return riga < pos.riga && colonna < pos.colonna;
+}
+
+Position Position::operator+(const Position& pos) const {
+    return Position(riga + pos.riga, colonna + pos.colonna);
+}
+
+Position Position::operator-(const Position& pos) const {
+    return Position(riga - pos.riga, colonna - pos.colonna);
+}
+
 void Position::operator=(const Position& pos) {
     riga = pos.riga;
     colonna = pos.colonna;
 }
 
-bool Position::operator!=(const Position& pos) {
-    return riga != pos.riga || colonna != pos.colonna;
+void Position::operator+=(const Position& pos) {
+    riga += pos.riga;
+    colonna += pos.colonna;
 }
 
-bool Position::operator==(const Position& pos) {
-    return riga == pos.riga && colonna == pos.colonna;
-}
-
-bool Position::operator<(const Position& pos) {
-    return riga < pos.riga && colonna < pos.colonna;
+void Position::operator-=(const Position& pos) {
+    riga -= pos.riga;
+    colonna -= pos.colonna;
 }
--- src/gamestruct/position.hpp
+++ src/gamestruct/position.hpp
@@ -1,12 +1,20 @@
 #pragma once
 
-//struttura che mantiene la posizione di un elemento nello spazio
+// struttura che mantiene la posizione di un elemento nello spazio
 struct Position {
     int riga;
     int colonna;
 
+    Position(int riga, int colonna);
+
+    bool operator!=(const Position& pos) const;
+    bool operator==(const Position& pos) const;
+    bool operator<(const Position& pos) const;
+
+    Position operator+(const Position& pos) const;
+    Position operator-(const Position& pos) const;
+
     void operator=(const Position& pos);
-    bool operator!=(const Position& pos);
-    bool operator==(const Position& pos);
-    bool operator<(const Position& pos);
+    void operator+=(const Position& pos);
+    void operator-=(const Position& pos);
 };
