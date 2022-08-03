75b7f42a334edd8ffbfab5eff0b4b4deca35cf54
Ruggi54 <simoruggi54@gmail.com>
2022-08-03 15:25:28 +0200

    Fix(Powers): Resolved memory leak in collisionDetection

--- src/entities/entity.cpp
+++ src/entities/entity.cpp
@@ -69,6 +69,7 @@ void Entity::move(manager::Level *levelManager) {
                 setPosition(Position(new_x, new_y));
                 break;
         }
+        delete collision;
     }
     
     this->direction_ = enums::Direction::NONE;
