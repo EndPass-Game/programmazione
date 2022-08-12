97b4ba4a6b3fd1d2b8154d3aca0b6e9a39822093
Ruggi54 <76696341+Ruggi54@users.noreply.github.com>
2022-08-12 22:30:34 +0200

    Feature(Player):Changed function "move" into an abstract one
    * Fix(Player): Moved the move function from entity to player
    
    * Fix(Player): move function became virtual in entity and did implement it in player
    
    * Fix(Player): removed libraries wich made the calls cicling
    
    * style: BOT STYLE FORMAT autocommit
    
    Co-authored-by: endpass-formatter <github-actions[bot]@users.noreply.github.com>

--- src/entities/player.cpp
+++ src/entities/player.cpp
@@ -17,6 +17,10 @@ Altro
 #include "entities/player.hpp"
 
 #include "entities/entity.hpp"
+#include "enums/collision-type.hpp"
+#include "enums/direction.hpp"
+#include "level/collidable.hpp"
+#include "manager/level.hpp"
 
 Player::Player()
     : Entity(12, 3,  // TODO: gestire queste costanti hardcoded in un file di setting
@@ -48,6 +52,47 @@ int Player::getPowers() {
     return powers_;
 }
 
+void Player::_handleDoorCollision(manager::Level *levelManager, level::DoorSegment *door, Position pos) {
+    logger_.info("collided with door");
+    if (door->getNextLevelIdx() == -1) {
+        int nextLevelIdx = levelManager->addLevel();
+        door->setNextLevelIdx(nextLevelIdx);
+    }
+
+    if (door->isDoorOpen()) {
+        logger_.info("moving to level with idx %d", door->getNextLevelIdx());
+        levelManager->goToLevel(door->getNextLevelIdx());
+    } else {
+        if (levelManager->getPlayer()->getPowers() > 0) {
+            logger_.info("opening door with idx %d", door->getNextLevelIdx());
+            levelManager->getPlayer()->removePower();
+            door->openDoor();  // temporaneo
+        }
+    }
+}
+
+void Player::_handleWallCollision(manager::Level *levelManager, level::WallSegment *wall, Position pos) {
+}
+
+void Player::_handleEntityCollision(manager::Level *levelManager, Entity *entity, Position pos) {
+}
+
+void Player::_handleArtifactCollision(manager::Level *levelManager, collectables::Artifact *artifact, Position pos) {
+    collectables::Artifact *art;
+    art = dynamic_cast<collectables::Artifact *>(artifact);
+    levelManager->getPlayer()->setLife(levelManager->getPlayer()->getLife() + art->getLifeUpgrade());
+    levelManager->getPlayer()->setPosition(pos);
+    delete art;
+}
+
+void Player::_handlePowerCollision(manager::Level *levelManager, collectables::Power *power, Position pos) {
+    levelManager->getPlayer()->addPower();
+    levelManager->getPlayer()->setPosition(pos);
+}
+
+void Player::_handleNoneCollision(manager::Level *levelManager, Position pos) {
+    levelManager->getPlayer()->setPosition(pos);
+}
 int Player::getScore() {
     return score_;
 }
