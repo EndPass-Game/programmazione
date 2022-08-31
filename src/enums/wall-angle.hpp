77417271af494abe38ee7b6b50da274107ca7322
Angelo Huang <xuanqiang.huang@studio.unibo.it>
2022-08-31 13:25:29 +0200

    docs(enums): add docs to enums

--- src/enums/wall-angle.hpp
+++ src/enums/wall-angle.hpp
@@ -1,12 +1,14 @@
 #pragma once
 
 namespace enums {
-
+    /**
+     * @brief Rappresenta una tipologia di angolo per WallSegment
+     * 
+     */
     enum class WallAngleType {
         NONE = 0,
         START = 1,
         END = 2,
         BOTH = 3,
     };
-
 };
